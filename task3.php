3523791ea83281271b4fe15eeb45c2648044b492
dicprozyg <dicprozug@gmail.com>
2018-07-24 01:32:54 +0300

    edited

--- task3.php
+++ task3.php
@@ -1,6 +1,12 @@
 <?php
 
-class Circle
+
+interface Figure 
+{
+    public function get_area();
+}
+
+class Circle implements Figure
 {
 
     public function __construct($radius)
@@ -13,10 +19,12 @@ class Circle
     {
         return $this->pi * $this->radius ^ 2; 
     }
+	
 }
 
-class Rectangle
+class Rectangle implements Figure
 {
+	
     public function __construct($a, $b)
     {
         $this->a = $a;
@@ -27,10 +35,12 @@ class Rectangle
     {
         return $this->a * $this->b; 
     }
+	
 }
 
-class Triangle
+class Triangle implements Figure
 {
+	
     public function __construct($a, $b, $c)
     {
         $this->a = $a;
@@ -43,31 +53,55 @@ class Triangle
         $p = ($this->a + $this->b + $this->c)/2;
         return $p * ($p - $this->a) * ($p - $this->b) * ($p - $this->c);
     }
+	
 }
 
-$json = '[{"type":"circle","radius":6},{"type":"rectangle","a":7,"b":5},{"type":"triangle","a":3,"b":5,"c":5},{"type":"rectangle","a":6,"b":8},{"type":"triangle","a":4,"b":5,"c":6},{"type":"circle","radius":7}]';
-$figures_data = json_decode($json, true);
+class Decode
+{
+	
+	public function decod($json)
+	{
+		$figures_data = json_decode($json, true);
+		return $figures_data;
+	}
+	
+}
 
-foreach($figures_data as $figure_data)
+
+class Sort
 {
-    if($figure_data['type'] == "circle")
-    {
-        $circle = new Circle($figure_data['radius']);
-        $result[] = $circle -> get_area();
-    }
-    elseif ($figure_data['type'] == "rectangle")
-    {
-        $rectangle = new Rectangle($figure_data['a'], $figure_data['b']);
-        $result[] = $rectangle -> get_area();
-    }
-    elseif ($figure_data['type'] == "triangle")
-    {
-        $triangle = new Triangle($figure_data['a'], $figure_data['b'], $figure_data['c']);
-        $result[] = $triangle -> get_area();
-    }
+	
+	public static function get_sort()
+	{
+		$c = new Decode();
+		$n = $c -> decod('[{"type":"circle","radius":6},{"type":"rectangle","a":7,"b":5},{"type":"triangle","a":3,"b":5,"c":5},{"type":"rectangle","a":6,"b":8},{"type":"triangle","a":4,"b":5,"c":6},{"type":"circle","radius":7}]');
+		
+		foreach($n as $figure_data)
+		{
+			if($figure_data['type'] == "circle")
+			{
+				$circle = new Circle($figure_data['radius']);
+				$result[] = $circle -> get_area();
+			}
+			elseif ($figure_data['type'] == "rectangle")
+			{
+				$rectangle = new Rectangle($figure_data['a'], $figure_data['b']);
+				$result[] = $rectangle -> get_area();
+			}
+			elseif ($figure_data['type'] == "triangle")
+			{
+				$triangle = new Triangle($figure_data['a'], $figure_data['b'], $figure_data['c']);
+				$result[] = $triangle -> get_area();
+			}
+		}
+		rsort($result);
+		foreach ($result as $key) 
+		{
+			echo $key."<br>";
+		}
+
+	}
 }
-rsort($result);
-foreach ($result as $key) 
-{
-    echo $key."\n";
-}
+
+$sort = new Sort();
+$sort = $sort -> get_sort();
